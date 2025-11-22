3932cc7e48e3fa912296f40a342189f72017041a
Elite6802 <kelvimutinda347@gmail.com>
2025-11-22 12:16:21 +0300

    feat: added some missing setions to the tables. Fixed minor bugs

--- user_auth/models.py
+++ user_auth/models.py
@@ -2,7 +2,20 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    ADMIN = 'admin'
+    CUSTOMER = 'customer'
+
+    ROLE_CHOICES = [
+        (ADMIN, 'Admin'),
+        (CUSTOMER, 'Customer'),
+    ]
+
     email = models.EmailField(unique=True)
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default=CUSTOMER,
+    )
 
     # Now that we have created the email, we use it as a login identifier and not the username
     USERNAME_FIELD = 'email'
